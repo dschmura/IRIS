9e363dbcb9b50c74b44b7dd98dae5704ae8b7e26
Jim Zajkowski <jamesez@umich.edu>
2012-05-16 23:46:20 -0400

    also precompile application-print.css

--- config/application.rb
+++ config/application.rb
@@ -51,5 +51,7 @@ module Iris
     # Version of your assets, change this if you want to expire all your assets
     config.assets.version = '1.0'
 
+    config.assets.precompile += ['application-print.css']
+
   end
 end
