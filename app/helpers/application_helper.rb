746f43f881f4f037d7e018631130a4a66ce0a20d
dschmura <dschmura@humbledaisy.com>
2012-06-18 08:33:34 -0400

    New MClassrooms logo and adjusted placement of U of M and LSA Logos in the footer.

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -14,13 +14,13 @@ module ApplicationHelper
   end
   
   def logo
-    image_tag("UniversityOfMichigan_wordmark.png", :alt => "University of Michigan Logo", :width => "400", :style => "padding: 1em;", :class => "round")
+    image_tag("MClassrooms_Logo.png", :alt => "University of Michigan Logo", :width => "250", :style => "padding: 1em;", :class => "round")
   end
   def footer_logo
    image_tag("LSA_Logo.png", :alt => "#{@base_title} is brought to you by LSA Instructional Support Services", :style => "height: 35px; float: left;")
   end
   def footer_logo_um
-   image_tag("UniversityOfMichigan_wordmark.png", :alt => "#{@base_title} is brought to you by LSA Instructional Support Services", :style => "height: 25px; float: right;")
+   image_tag("UniversityOfMichigan_wordmark.png", :alt => "#{@base_title} is brought to you by LSA Instructional Support Services", :style => "height: 25px; float: right; padding-top: 5px;")
   end
   
   def construction_sign
