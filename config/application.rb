4d86480913caa3dbce0250cd454917aa5dd45360
dschmura <dschmura@humbledaisy.com>
2012-07-17 12:46:11 -0400

    Remove rack_rewrite. no really.

--- config/application.rb
+++ config/application.rb
@@ -1,8 +1,6 @@
 require File.expand_path('../boot', __FILE__)
 
 require 'rails/all'
-require 'rack-rewrite'
-
 
 
 if defined?(Bundler)
--- config/initializers/rack_rewrite.rb
+++
@@ -1,5 +0,0 @@
-Iris::Application.config.middleware.insert_before(Rack::Lock, Rack::Rewrite) do
-
-
-
-end
