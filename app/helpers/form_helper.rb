cdba4729390a75518e24023785cfb3bdc2c03e3b
David Chmura <dschmura@humbledaisy.com>
2013-02-11 11:52:25 -0500

    more kruft

--- app/helpers/form_helper.rb
+++
@@ -1,8 +0,0 @@
-# app/helpers/form_helper.rb
-module FormHelper
-  def setup_location(location)
-    location.building  ||= Building.new
-    location
-  end
-    
-end
--- app/helpers/old_site_redirector_helper.rb
+++
@@ -1,2 +0,0 @@
-module OldSiteRedirectorHelper
-end
