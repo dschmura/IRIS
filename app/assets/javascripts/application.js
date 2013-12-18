1ca5e32f9f01c0ee9460df76ef3590ab6016be8c
David Chmura <dschmura@humbledaisy.com>
2013-12-18 10:22:00 -0500

    Added respond.js for IE8 bootstrap support

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -10,6 +10,7 @@
 // WARNING: THE FIRST BLANK LINE MARKS THE END OF WHAT'S TO BE PROCESSED, ANY BLANK LINE SHOULD
 // GO AFTER THE REQUIRES BELOW.
 //
+//= require respond
 //= require jquery
 //= require jquery-ui
 //= require jquery.ui.touch-punch.min
