80737d9124a72b32a752f267188ce1c0beda58cc
David Chmura <dschmura@humbledaisy.com>
2014-02-07 10:50:03 -0500

    rollback

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -37,8 +37,6 @@ Iris::Application.configure do
   
   config.action_mailer.default_url_options = { :host => 'localhost:3000' }
   
-  config.after_initialize do
-    require "prawn"
-  end
+  
 end
 
