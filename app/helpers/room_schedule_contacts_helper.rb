7b9d3b8aec70825fba938657af52264810b7a777
Dave Chmura <dschmura@humbledaisy.com>
2015-07-27 14:16:28 -0400

    initial implimentation of quicklink feature

--- app/helpers/room_schedule_contacts_helper.rb
+++ app/helpers/room_schedule_contacts_helper.rb
@@ -1,2 +1,25 @@
 module RoomScheduleContactsHelper
+  def scheduling_contact_email
+    if @room_schedule_contact.RM_SPPT_CNTCT_EMAIL
+      mail_to @room_schedule_contact. RM_SPPT_CNTCT_EMAIL.to_s, "Email Scheduler"
+    else
+      "Scheduling Contact N/A"
+    end
+  end
+
+  def support_contact_email
+    if @room_schedule_contact.RM_SPPT_CNTCT_EMAIL
+      mail_to @room_schedule_contact.RM_SPPT_CNTCT_EMAIL.to_s, "Email Support"
+    else
+      "Support Contact N/A"
+    end
+  end
+
+  def floor_plan
+    if  File.exist?("#{Rails.root}/public/maps/#{basement}.html")
+      link_to "Floor Map", "../maps/#{basement}.html"
+    else
+      "Floor Map N/A"
+    end
+  end
 end
