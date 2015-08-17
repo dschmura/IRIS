b28f6b762dcf427f86126659a82fb8393849bb20
dschmura <dschmura@Davids-MacBook-Pro.local>
2015-08-16 20:28:03 -0400

    line change

--- config/environments/test.rb
+++ config/environments/test.rb
@@ -9,7 +9,7 @@ Iris::Application.configure do
 
   # Log error messages when you accidentally call methods on nil.
   config.whiny_nils = false
-
+  config.eager_load = false
   # Show full error reports and disable caching
   config.consider_all_requests_local       = true
   config.action_controller.perform_caching = false
