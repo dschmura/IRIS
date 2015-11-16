8759f804c3568c08f7d51e13c220c391c548aa60
David Chmura <dschmura@humbledaisy.com>
2015-11-16 14:05:08 -0500

    Add pluck method to index

--- app/controllers/api/v1/classrooms_controller.rb
+++ app/controllers/api/v1/classrooms_controller.rb
@@ -1,11 +1,13 @@
 class Api::V1::ClassroomsController < ApplicationController
   def show
     @classroom = Classroom.find_by facility_code_heprod:(params[:facility_code_heprod].upcase)
+    @classroom = @classroom.to_json
     render json: @classroom
   end
 
   def index
-    @classrooms = Classroom.all
+    #@classrooms = Classroom.all
+    @classrooms = Classroom.pluck(:facility_code_heprod, :student_capacity)
     render json: @classrooms
   end
 end
