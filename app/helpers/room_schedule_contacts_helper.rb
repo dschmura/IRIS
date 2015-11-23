91ac102c963f533dc1c3a395084fad9b03a95861
David Chmura <dschmura@humbledaisy.com>
2015-11-23 10:43:05 -0500

    Fix scheduling_contact_email helper method

--- app/helpers/room_schedule_contacts_helper.rb
+++ app/helpers/room_schedule_contacts_helper.rb
@@ -1,7 +1,7 @@
 module RoomScheduleContactsHelper
 
   def scheduling_contact_email
-    if @room_schedule_contact.RM_SPPT_CNTCT_EMAIL.present?
+    if @room_schedule_contact.rm_schd_email.present?
       mail_to @room_schedule_contact. rm_schd_email.to_s, truncate(@room_schedule_contact.rm_schd_cntct_name, :length => 40)
     else
       "Scheduling Contact Not Available"
