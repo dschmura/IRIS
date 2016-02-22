74c0f5db41ec7fdc0d68ae7e4218adb5be32ad69
David Chmura <dschmura@humbledaisy.com>
2016-02-22 14:35:35 -0500

    Fix room_schedule_contact importer

--- app/models/room_schedule_contact.rb
+++ app/models/room_schedule_contact.rb
@@ -34,8 +34,7 @@ class RoomScheduleContact < ActiveRecord::Base
   def self.import(file)
     CSV.foreach(file.path, headers: true) do |row|
       roomschedulecontact = find_by_rmrecnbr(row["RMRECNBR"]) || new
-      roomschedulecontact.attributes = row.to_hash.slice(*accessible_attributes)
-      roomschedulecontact.save!
+      roomschedulecontact.update_attributes(rm_schd_cntct_name: row["RM_SCHD_CNTCT_NAME"], rm_schd_email: row["RM_SCHD_EMAIL"], rm_schd_cntct_phone: row["RM_SCHD_CNTCT_PHONE"], rm_det_url: row["RM_DET_URL"], rm_usage_guidlns_url:  row["RM_USAGE_GUIDLNS_URL"], RM_SPPT_DEPTID: row["RM_SPPT_DEPTID"], RM_SPPT_DEPT_DESCR: row["RM_SPPT_DEPT_DESCR"], RM_SPPT_CNTCT_EMAIL: row["RM_SPPT_CNTCT_EMAIL"] , RM_SPPT_CNTCT_PHONE: row["RM_SPPT_CNTCT_PHONE"] , RM_SPPT_CNTCT_URL: row["RM_SPPT_CNTCT_URL"])
     
     end
     
